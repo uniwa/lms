dbfcfdd88ed7e5c0a26a5d59d5e7a577f83d590c
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-03-16 19:31:26 +0200

    Further improvements to capifony

--- Capfile
+++ Capfile
@@ -1,5 +1,5 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
-#load 'vendor/ruudk/capifony-tools/load.rb'
+load 'vendor/ruudk/capifony-tools/load.rb'
 
 require 'capifony_symfony2'
 load 'app/config/deploy'
--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -22,8 +22,42 @@ set  :dump_assetic_assets, true
 set  :interactive_mode, false
 set  :keep_releases,  3
 
-set :shared_files, ["app/config/parameters.ini"]
+set :shared_files, ["app/config/parameters.yml"]
 set :shared_children, [app_path + "/logs", web_path + "/upload", web_path + "/cache"]
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
+
+# Hooks
+after  "symfony:assetic:dump", "symfony:doctrine:schema:update" # Update doctrine schema
+after  "symfony:assetic:dump", "symfony:update_admin_acl"
+
+namespace :symfony do
+  desc "Runs the test suite"
+  task :run_tests, :roles => :app, :except => { :no_release => true } do
+    capifony_pretty_print "--> Clearing test environment cache"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} cache:clear --env=test --no-debug'"
+    capifony_puts_ok
+    capifony_pretty_print "--> Running test suite"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} doctrine:schema:update --force --env=test'"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} doctrine:fixtures:load --env=test --no-interaction'"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && phpunit -c #{app_path} src'"
+    capifony_puts_ok
+  end
+  desc "Import locale"
+  task :import_locale, :roles => :app, :except => { :no_release => true } do
+    capifony_pretty_print "--> Importing translation files"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} locale:editor:import --env=#{symfony_env_prod}'"
+    capifony_puts_ok
+  end
+  desc "Update admin acl"
+  task :update_admin_acl, :roles => :app, :except => { :no_release => true } do
+    capifony_pretty_print "--> Updating admin ACL"
+    run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} sonata:admin:setup-acl --env=#{symfony_env_prod}'"
+    capifony_puts_ok
+  end
+  task :update_acl, :roles => :app do
+  end
+  task :apc, :roles => :app do
+  end
+end
