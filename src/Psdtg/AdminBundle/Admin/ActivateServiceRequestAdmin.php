fd0b0e46c7c6488cb5858caa02258e7b12be9056
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-10-08 22:53:35 +0300

    Finalized ActivateServiceRequest

--- src/Psdtg/AdminBundle/Admin/ActivateServiceRequestAdmin.php
+++ src/Psdtg/AdminBundle/Admin/ActivateServiceRequestAdmin.php
@@ -1,6 +1,15 @@
 <?php
 namespace Psdtg\AdminBundle\Admin;
 
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
 class ActivateServiceRequestAdmin extends ExistingCircuitRequestAdmin
 {
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        parent::configureListFields($listMapper);
+        $listMapper
+            ->add('newConnectivityType.name', 'trans')
+        ;
+    }
 }
